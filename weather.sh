52e00fce76e9d47d8f9d1412b9bbe3c622ac613b
YeLee <maishangming@126.com>
2012-05-12 08:59:31 +0800

    修改一下weather.sh

--- weather.sh
+++ weather.sh
@@ -2,4 +2,4 @@
 CITYCODE=101280802
 #http://wap.weather.com.cn/wap
 #先用上面的链接查询城市代码
-wget http://wap.weather.com.cn/wap/weather/$CITYCODE.shtml -q -O -|egrep -B1 -e ℃  -e strong -e 发布 |egrep -v "\-\-|div"|sed  -e 's/<.[0-9a-z]*>//g' -e 's/<br \/>//g' -e 's/<img src="http:\/\/www\.weather\.com\.cn\/m\/i\/icon_weather\/21x15\/[a-z][0-9][0-9]\.gif" \/>//g' -e 's/\(^[0-9]*\)-\([0-9]*\)-\([0-9]*\)/\1年\2月\3日/g' -e 's/[[:space:]]//g' -e "/^$/d"|sed -e '$!N;s/\n/ /' -e 's/ \([^x00-xff]*\)\([0-9]*℃\/[0-9]*℃\)&nbsp;\([^x00-xff]*\)/\t\3\t\2\t\1/g' -e 's/℃/℃ /g'
+wget http://wap.weather.com.cn/wap/weather/$CITYCODE.shtml -q -O -|egrep -B1 -e ℃  -e strong -e 发布 |egrep -v "\-\-|div"|sed -e 's/<[^>]*>//g' -e 's/\(^[0-9]*\)-\([0-9]*\)-\([0-9]*\)/\1年\2月\3日/g' -e 's/[[:space:]]//g'|sed -e '$!N;s/\n/ /' -e 's/ \([^x00-xff]*\)\([0-9]*℃\/[0-9]*℃\)&nbsp;\([^x00-xff]*\)/ \t\3 \t\2 \t\1/g' -e 's/℃/℃ /g'
