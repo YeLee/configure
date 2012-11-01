f0e0b305876a0529399329adcb3de7edadb54200
YeLee <maishangming@126.com>
2012-11-01 08:25:20 +0800

    更新壁纸脚本

--- fvwm/wallpaper.sh
+++ fvwm/wallpaper.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-intertime=300
-if [ -e /tmp/wallpaper ];then
+intertime=1800
+wallpaper=/tmp/wallpaper
+if [ -e $wallpaper  ];then
 	exit 0
 else
-	touch /tmp/wallpaper
+	touch $wallpaper
 fi
 
 signal_handle()
 {
-	rm /tmp/wallpaper
+	rm $wallpaper
 	exit 1
 }
 trap signal_handle SIGHUP SIGINT SIGQUIT SIGABRT SIGKILL SIGALRM SIGTERM
-	
+
 while true
 do
 	for i in $(find $1 -type f -name "*.png")
@@ -20,8 +21,9 @@ do
 		fvwm-root --retain-pixmap $i
 		sleep $intertime
 		if [ -z $(pgrep fvwm) ];then
-			rm /tmp/wallpaper
+			rm $wallpaper
 			exit 0
 		fi
 	done
 done
+
