a5a61420d2015da91d4be4231fb7467a0a3e7a70
YeLee <maishangming@126.com>
2012-12-20 11:31:02 +0800

    修正flash脚本目标目录不存在的问题

--- dlflash.sh
+++ dlflash.sh
@@ -14,5 +14,6 @@ fi
 
 wget -q --user-agent="Mozilla/5.0" "http://get.adobe.com/cn/flashplayer/completion/?installer=Flash_Player_11.2_for_other_Linux_(.tar.gz)_$OS_BIT-bit" -O - |grep -o "http.*tar.gz" |axel - -o $tempdir/install_flash_player_11_linux.tar.gz
 tar xf $tempdir/install_flash_player_11_linux.tar.gz -C $tempdir
+mkdir -p ~/.mozilla/plugins
 cp $tempdir/libflashplayer.so ~/.mozilla/plugins 
 rm -rf $tempdir
