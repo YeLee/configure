27f09ff8f8941f93dc11a0c35231cb5b0b3cfeec
YeLee <maishangming@126.com>
2013-01-24 14:37:38 +0800

    Update letvdl.sh
    修正flvcd更新后视频无法下载的问题。

--- letvdl.sh
+++ letvdl.sh
@@ -9,7 +9,7 @@ i=$(ls *.flv.st|sed 's/\([0-9]*\).flv.st/\1/g')
 while [ $i -le $ipart ]
 do
 	echo "Start the download Episode $i ..."
-	wget -q --user-agent="Mozilla/5.0" "http://www.flvcd.com/parse.php?kw=http://www.letv.com/ptv/pplay/$tvid/$i.html&format=$iformat" -O - |sed -ne 's/^<U>//p' |axel - --user-agent="Mozilla/5.0" -o $i.flv
+	wget -q --user-agent="Mozilla/5.0" "http://www.flvcd.com/parse.php?kw=http://www.letv.com/ptv/pplay/$tvid/$i.html&format=$iformat" -O - |grep clipurl |grep -o 'http[^"]*' |axel - --user-agent="Mozilla/5.0" -o $i.flv
 	let "i+=1"
 done
 
