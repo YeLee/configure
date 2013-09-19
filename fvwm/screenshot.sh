4483a1cf67826ed50e70f932191075ffd635acc6
YeLee <maishangming@126.com>
2013-09-19 11:43:42 +0800

    更新fvwm与rime的配置文件

--- fvwm/screenshot.sh
+++ fvwm/screenshot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-for((i=10;i>=1;i--));do notify-send -t 800 剩余$i秒…… && sleep 1;done
+for i in {10..1};do notify-send -t 800 剩余${i}秒…… && sleep 1;done
 scrot '%Y-%m-%d_%H%M%S_$wx$h.jpg' -e 'mv $f ~/图片/'
+notify-send -t 5000 截图已保存到\"${HOME}/图片\"下面。
+
